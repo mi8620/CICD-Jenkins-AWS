c801f125249ce18a1e386b9e40d1e1127ab0ee6e
Adrta Docker <mihirhandel862@gmail.com>
2026-02-27 19:46:57 +0530

    adrta

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,9 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            adrta mi {" "}
+            adrtas mi {" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/me.tsx
+              app/me/page.tsx
             </code>
             .
           </li>
